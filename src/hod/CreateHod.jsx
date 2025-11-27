6a675cd8499dc2f87713267d1847f20ebff4983b
Shailesh Gokhale <68338746+shailesh-ss-19-11@users.noreply.github.com>
2025-11-27 21:27:06 +0530

    added formik in form

--- src/hod/CreateHod.jsx
+++ src/hod/CreateHod.jsx
@@ -1,69 +1,83 @@
-import React, { useState } from 'react'
 import { useFormik } from 'formik'
+import * as Yup from 'yup';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 const CreateHod = () => {
     const navigate = useNavigate()
-    const [formInput, setformInput] = useState({ email: "", phone: "", name: "" });
 
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        let obj = { ...formInput };
-        obj[name] = value
-        setformInput(obj);
-    }
+    const phoneRegExp = /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        try {
-            const response = await axios.post("http://localhost:3000/api/admin/hod", formInput);
-            if (response.status >= 200 && response.status < 300) {
-                navigate("/hod")
+    const validationSchema = Yup.object({
+        name: Yup.string().max(50).required("Name is Required"),
+        email: Yup.string().email().max(50).required("Email is required"),
+        phone: Yup.string().matches(phoneRegExp, "Phone number is not valid")
+    })
+
+    const formik = useFormik({
+        validationSchema,
+        initialValues: {
+            name: "",
+            email: "",
+            phone: null
+        },
+        onSubmit: async (values) => {
+            try {
+                const response = await axios.post("http://localhost:3000/api/admin/hod", values);
+                if (response.status >= 200 && response.status < 300) {
+                    navigate("/hod")
+                }
+            } catch (err) {
+                console.log(err);
             }
-        } catch (err) {
-            console.log(err);
         }
-    }
+    })
 
 
     return (
         <div class="container mt-4">
-            <form onSubmit={handleSubmit} class="p-4 border rounded shadow-sm bg-white">
 
-                <div class="mb-3">
-                    <label class="form-label">Name</label>
-                    <input
-                        type="text"
-                        class="form-control"
-                        name="name"
-                        onChange={handleChange}
-                    />
-                </div>
+            <div class="mb-3">
+                <label class="form-label">Name</label>
+                <input
+                    name="name"
+                    value={formik.values.name}
+                    type="text"
+                    class="form-control"
+                    onChange={formik.handleChange}
+                />
+                <p className='text-danger'>{formik.touched && formik.errors.name}</p>
+            </div>
 
-                <div class="mb-3">
-                    <label class="form-label">Email</label>
-                    <input
-                        type="email"
-                        class="form-control"
-                        name="email"
-                        onChange={handleChange}
-                    />
-                </div>
 
-                <div class="mb-3">
-                    <label class="form-label">Phone</label>
-                    <input
-                        type="text"
-                        class="form-control"
-                        name="phone"
-                        onChange={handleChange}
-                    />
-                </div>
+            <div class="mb-3">
+                <label class="form-label">Email</label>
+                <input
+                    name="email"
+                    value={formik.values.email}
+                    type="email"
+                    class="form-control"
+                    onChange={formik.handleChange}
 
-                <button type="submit" class="btn btn-primary w-100">
-                    Submit
-                </button>
-            </form>
+                />
+                <p className='text-danger'>{formik.touched.email && formik.errors.email}</p>
+            </div>
+
+            <div class="mb-3">
+                <label class="form-label">Phone</label>
+                <input
+                    name="phone"
+                    value={formik.values.phone}
+                    type="text"
+                    class="form-control"
+                    onChange={formik.handleChange}
+                    required
+                />
+                <p className='text-danger'>{formik.touched.phone && formik.errors.phone}</p>
+            </div>
+
+            <button onClick={formik.submitForm} class="btn btn-primary w-100">
+                Submit
+            </button>
         </div>
 
     )
